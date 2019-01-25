c43de0a3107b255de8e28c551a7f9f873e11390b
HJK <HJKdev@gmail.com>
2019-01-25 14:52:07 +0800

    新增FLAC无损音乐支持

--- utils/echo.py
+++ utils/echo.py
@@ -63,7 +63,7 @@ def usage():
     print('\t%-16s %s' % ('-m --merge ', '对搜索结果去重和排序'))
     print('\t%-16s %s' % ('--nomerge ', '对搜索结果不去重（默认不去重）'))
     print('\t%-16s %s' % ('-k --keyword= ', '搜索关键字'))
-    print('\t%-16s %s' % ('-s --source= ', '数据源目前支持qq netease kugou baidu xiami'))
+    print('\t%-16s %s' % ('-s --source= ', '数据源目前支持qq netease kugou baidu xiami flac'))
     print('\t%-16s %s' % ('-c --count= ', '数量限制'))
     print('\t%-16s %s' % ('-o --outdir= ', '指定输出目录'))
     print('example: python main.py -k "周杰伦" -s "qq netease kugou baidu xiami" -c 10 -o "/tmp"')
