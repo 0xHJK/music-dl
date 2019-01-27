60381056b89b58afe95713a213b03b8b5d8b219c
HJK <HJKdev@gmail.com>
2019-01-28 00:09:38 +0800

    删除已被替代的函数

--- music_dl/utils/echo.py
+++ music_dl/utils/echo.py
@@ -9,7 +9,6 @@
 
 """
 import platform
-from .. import config
 
 colors = {
     'red': '\033[91m',
@@ -34,40 +33,6 @@ def colorize(string, color):
         return string
     return colors[color] + string + '\033[0m'
 
-
-def info(music):
-    ''' 打印歌曲信息 '''
-    if not config.get('verbose'):
-        return
-    s = '\n ------------ \n -> 歌曲：%s\n -> 歌手：%s\n -> 时长: %s\n -> 大小: %s\n -> 比特率: %s\n -> URL: %s\n' % \
-        (music['title'], music['singer'], music['duration'], music['size'], music['rate'], music['url'])
-    print(s)
-
-
-def menu(music_list):
-    ''' 打印歌曲列表信息 '''
-    for music in music_list:
-        idx = colorize(' [ %2s ] ' % music_list.index(music), 'cyan')
-        source = colorize('%7s | ' % music['source'].upper(), music['source'])
-        size = colorize('%5sMB' % music['size'], 'yellow')
-        title = colorize(music['title'], 'yellow')
-        info = '%s - %s - %s - %s - %s' % \
-               (music['duration'], size, music['singer'], title, music['album'])
-
-        print(idx + source + info)
-
-def usage():
-    print('usage: python main.py [-k keyword] [-s source] [-c count] [-o outdir] [-v] [-m]')
-    print('\t%-16s %s' % ('-h --help ', '帮助'))
-    print('\t%-16s %s' % ('-v --verbose ', '详细模式'))
-    print('\t%-16s %s' % ('-m --merge ', '对搜索结果去重和排序'))
-    print('\t%-16s %s' % ('--nomerge ', '对搜索结果不去重（默认不去重）'))
-    print('\t%-16s %s' % ('-k --keyword= ', '搜索关键字'))
-    print('\t%-16s %s' % ('-s --source= ', '数据源目前支持qq netease kugou baidu xiami flac'))
-    print('\t%-16s %s' % ('-c --count= ', '数量限制'))
-    print('\t%-16s %s' % ('-o --outdir= ', '指定输出目录'))
-    print('\t%-16s %s' % ('-x --proxy= ', '指定代理（如http://127.0.0.1:1087）'))
-    print('example: python main.py -k "周杰伦" -s "qq netease kugou baidu xiami" -c 10 -o "/tmp"')
 
 
 def notice(keyword):
