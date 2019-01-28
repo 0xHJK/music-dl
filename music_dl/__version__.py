6f308d97ce97ca1102dbe299db9af1eae6c21044
HJK <HJKdev@gmail.com>
2019-01-28 10:37:24 +0800

    新增version文件

---
+++ music_dl/__version__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python  
+#-*- coding:utf-8 _*-  
+"""
+@author: HJK 
+@file: __version__.py 
+@time: 2019-01-28
+
+"""
+
+__title__ = 'pymusic-dl'
+__description__ = 'Search and download music from netease, qq, kugou, baidu and xiami.'
+__url__ = 'https://github.com/0xHJK/music-dl'
+__version__ = '2.0.0'
+__author__ = 'HJK'
+__author_email__ = 'HJKdev@gmail.com'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2019 HJK'
