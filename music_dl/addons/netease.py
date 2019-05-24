3c437bc82c78a5581a92d2f321c2018619a7e16e
HJK <HJKdev@gmail.com>
2019-05-24 15:34:37 +0800

    统一网易云音乐请求接口

--- music_dl/addons/netease.py
+++ music_dl/addons/netease.py
@@ -17,6 +17,8 @@ from .. import config
 from ..exceptions import RequestError, ResponseError, DataError
 from ..song import BasicSong
 
+__all__ = ["search", "playlist"]
+
 
 class NeteaseSong(BasicSong):
     def __init__(self):
@@ -24,90 +26,80 @@ class NeteaseSong(BasicSong):
 
     def download_lyrics(self):
         row_data = {"csrf_token": "", "id": self.id, "lv": -1, "tv": -1}
-        data = encrypted_request(row_data)
-        s = requests.Session()
-        s.headers.update(config.get("fake_headers"))
-        if config.get("proxies"):
-            s.proxies.update(config.get("proxies"))
-        s.headers.update({"referer": "http://music.163.com/"})
-        r = s.post("https://music.163.com/weapi/song/lyric", data=data)
-        json_data = r.json()
+        data = NeteaseApi.encrypted_request(row_data)
 
-        if "lrc" in json_data and "lyric" in json_data["lrc"]:
-            self.lyrics_text = json_data["lrc"]["lyric"]
+        self.lyrics_text = (
+            NeteaseApi.request(
+                "https://music.163.com/weapi/song/lyric", method="POST", data=data
+            )
+            .get("lrc", {})
+            .get("lyric", "")
+        )
+
+        if self.lyrics_text:
             super(NeteaseSong, self)._save_lyrics_text()
 
     def download(self):
         """ Download song from netease music """
-        data = encrypted_request(dict(ids=[self.id], br=32000))
-        s = requests.Session()
-        s.headers.update(config.get("fake_headers"))
-        if config.get("proxies"):
-            s.proxies.update(config.get("proxies"))
-        s.headers.update({"referer": "http://music.163.com/"})
+        data = NeteaseApi.encrypted_request(dict(ids=[self.id], br=32000))
+        res_data = NeteaseApi.request(
+            "http://music.163.com/weapi/song/enhance/player/url",
+            method="POST",
+            data=data,
+        ).get("data", [])
 
-        r = s.post("http://music.163.com/weapi/song/enhance/player/url", data=data)
-        if r.status_code != requests.codes.ok:
-            raise RequestError(r.text)
-        j = r.json()
-        if j["code"] != 200:
-            raise ResponseError(r.text)
-
-        self.song_url = j["data"][0]["url"]
-        self.rate = int(j["data"][0]["br"] / 1000)
+        if len(res_data) > 0:
+            self.song_url = res_data[0].get("url", "")
+            self.rate = int(res_data[0].get("br", 0) / 1000)
 
         super(NeteaseSong, self).download()
 
 
 def netease_search(keyword) -> list:
-    """ 从网易云音乐搜索 """
+    """ Search song from netease music """
     number = config.get("number") or 5
     eparams = {
         "method": "POST",
         "url": "http://music.163.com/api/cloudsearch/pc",
         "params": {"s": keyword, "type": 1, "offset": 0, "limit": number},
     }
-    data = {"eparams": encode_netease_data(eparams)}
-
-    s = requests.Session()
-    s.headers.update(config.get("fake_headers"))
-    if config.get("proxies"):
-        s.proxies.update(config.get("proxies"))
-    s.headers.update({"referer": "http://music.163.com/"})
-
-    r = s.post("http://music.163.com/api/linux/forward", data=data)
-    if r.status_code != requests.codes.ok:
-        raise RequestError(r.text)
-    j = r.json()
-    if j["code"] != 200:
-        raise ResponseError(r.text)
+    data = {"eparams": NeteaseApi.encode_netease_data(eparams)}
 
     songs_list = []
+    res_data = (
+        NeteaseApi.request(
+            "http://music.163.com/api/linux/forward", method="POST", data=data
+        )
+        .get("result", {})
+        .get("songs", {})
+    )
     try:
-        for m in j["result"]["songs"]:
-            if m["privilege"]["fl"] == 0:
+        for item in res_data:
+            if item.get("privilege", {}).get("fl", {}) == 0:
                 # 没有版权
                 continue
             # 获得歌手名字
-            singers = [s["name"] for s in m["ar"]]
-            # 获得最优音质的文件大小
-            if m["privilege"]["fl"] >= 320000 and "h" in m and m["h"]:
-                # 有时候即使>=320000，h属性依然为None
-                size = m["h"]["size"]
-            elif m["privilege"]["fl"] >= 192000 and "m" in m and m["m"]:
-                size = m["m"]["size"]
+            singers = [s.get("name", "") for s in item.get("ar", [])]
+            # 获得音乐的文件大小
+            # TODO: 获取到的大小并不准确，考虑逐一获取歌曲详情
+            if item.get("privilege", {}).get("fl", {}) >= 320000 and item.get("h", ""):
+                size = item.get("h", {}).get("size", 0)
+            elif item.get("privilege", {}).get("fl", {}) >= 192000 and item.get(
+                "m", ""
+            ):
+                size = item.get("m", {}).get("size", 0)
             else:
-                size = m["l"]["size"]
+                size = item.get("l", {}).get("size", 0)
 
             song = NeteaseSong()
             song.source = "netease"
-            song.id = m["id"]
-            song.title = m["name"]
+            song.id = item.get("id", "")
+            song.title = item.get("name", "")
             song.singer = "、".join(singers)
-            song.album = m["al"]["name"]
-            song.duration = int(m["dt"] / 1000)
+            song.album = item.get("al", {}).get("name", "")
+            song.duration = int(item.get("dt", 0) / 1000)
             song.size = round(size / 1048576, 2)
-            song.cover_url = m["al"]["picUrl"]
+            song.cover_url = item.get("al", {}).get("picUrl", "")
             songs_list.append(song)
     except Exception as e:
         raise DataError(e)
@@ -115,91 +107,115 @@ def netease_search(keyword) -> list:
     return songs_list
 
 
-def encode_netease_data(data) -> str:
-    data = json.dumps(data)
-    key = binascii.unhexlify("7246674226682325323F5E6544673A51")
-    encryptor = AES.new(key, AES.MODE_ECB)
-    # 补足data长度，使其是16的倍数
-    pad = 16 - len(data) % 16
-    fix = chr(pad) * pad
-    byte_data = (data + fix).encode("utf-8")
-    return binascii.hexlify(encryptor.encrypt(byte_data)).upper().decode()
-
-
-def encrypted_request(data) -> dict:
-    MODULUS = (
-        "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7"
-        "b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280"
-        "104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932"
-        "575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b"
-        "3ece0462db0a22b8e7"
-    )
-    PUBKEY = "010001"
-    NONCE = b"0CoJUm6Qyw8W8jud"
-    data = json.dumps(data).encode("utf-8")
-    secret = create_key(16)
-    params = aes(aes(data, NONCE), secret)
-    encseckey = rsa(secret, PUBKEY, MODULUS)
-    return {"params": params, "encSecKey": encseckey}
-
-
-def aes(text, key):
-    pad = 16 - len(text) % 16
-    text = text + bytearray([pad] * pad)
-    encryptor = AES.new(key, 2, b"0102030405060708")
-    ciphertext = encryptor.encrypt(text)
-    return base64.b64encode(ciphertext)
-
-
-def rsa(text, pubkey, modulus):
-    text = text[::-1]
-    rs = pow(int(binascii.hexlify(text), 16), int(pubkey, 16), int(modulus, 16))
-    return format(rs, "x").zfill(256)
-
-
-def create_key(size):
-    return binascii.hexlify(os.urandom(size))[:16]
-
-
 def netease_playlist(url) -> list:
     songs_list = []
     playlist_id = re.findall(r".+playlist\\*\?id\\*=(\d+)", url)[0]
     if playlist_id:
-        print(playlist_id)
         params = dict(
             id=playlist_id, total="true", limit=1000, n=1000, offest=0, csrf_token=""
         )
-        data = encrypted_request(params)
-        s = requests.Session()
-        s.headers.update(config.get("fake_headers"))
-        if config.get("proxies"):
-            s.proxies.update(config.get("proxies"))
-        s.headers.update({"referer": "http://music.163.com/"})
-        r = s.post("http://music.163.com/weapi/v3/playlist/detail", data=data)
-        j = r.json()
+        data = NeteaseApi.encrypted_request(params)
 
-        for track in j["playlist"]["tracks"]:
+        res_data = (
+            NeteaseApi.request(
+                "http://music.163.com/weapi/v3/playlist/detail",
+                method="POST",
+                data=data,
+            )
+            .get("playlist", {})
+            .get("tracks", [])
+        )
+        for item in res_data:
             song = NeteaseSong()
             # 获得歌手名字
-            singers = [s["name"] for s in track["ar"]]
-            # 获得最优音质的文件大小
-            if "h" in track and track["h"]:
-                # 有时候即使>=320000，h属性依然为None
-                size = track["h"]["size"]
-            elif "m" in track and track["m"]:
-                size = track["m"]["size"]
+            singers = [s.get("name", "") for s in item.get("ar", {})]
+            # 获得音乐文件大小
+            # TODO: 获取到的大小并不准确，考虑逐一获取歌曲详情
+            if item.get("l", ""):
+                size = item.get("l", {}).get("size", 0)
+            elif item.get("m", ""):
+                size = item.get("m", {}).get("size", 0)
             else:
-                size = track["l"]["size"]
+                size = item.get("h", {}).get("size", 0)
             song.source = "netease"
-            song.id = track["id"]
-            song.title = track["name"]
+            song.id = item.get("id", "")
+            song.title = item.get("name", "")
             song.singer = "、".join(singers)
-            song.album = track["al"]["name"]
-            song.duration = int(track["dt"] / 1000)
+            song.album = item.get("al", {}).get("name", "")
+            song.duration = int(item.get("dt", 0) / 1000)
             song.size = round(size / 1048576, 2)
-            song.cover_url = track["al"]["picUrl"]
+            song.cover_url = item.get("al", {}).get("picUrl", "")
             songs_list.append(song)
     return songs_list
+
+
+class NeteaseApi:
+    """ Netease music api http://music.163.com """
+
+    # class property
+    session = requests.Session()
+    session.headers.update(config.get("fake_headers"))
+    if config.get("proxies"):
+        session.proxies.update(config.get("proxies"))
+    session.headers.update({"referer": "http://music.163.com/"})
+
+    @classmethod
+    def request(cls, url, method="POST", data=None):
+        if method == "GET":
+            resp = cls.session.get(url, params=data)
+        else:
+            resp = cls.session.post(url, data=data)
+        if resp.status_code != requests.codes.ok:
+            raise RequestError(resp.text)
+        if not resp.text:
+            raise ResponseError("No response data.")
+        return resp.json()
+
+    @classmethod
+    def encode_netease_data(cls, data) -> str:
+        data = json.dumps(data)
+        key = binascii.unhexlify("7246674226682325323F5E6544673A51")
+        encryptor = AES.new(key, AES.MODE_ECB)
+        # 补足data长度，使其是16的倍数
+        pad = 16 - len(data) % 16
+        fix = chr(pad) * pad
+        byte_data = (data + fix).encode("utf-8")
+        return binascii.hexlify(encryptor.encrypt(byte_data)).upper().decode()
+
+    @classmethod
+    def encrypted_request(cls, data) -> dict:
+        MODULUS = (
+            "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7"
+            "b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280"
+            "104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932"
+            "575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b"
+            "3ece0462db0a22b8e7"
+        )
+        PUBKEY = "010001"
+        NONCE = b"0CoJUm6Qyw8W8jud"
+        data = json.dumps(data).encode("utf-8")
+        secret = cls.create_key(16)
+        params = cls.aes(cls.aes(data, NONCE), secret)
+        encseckey = cls.rsa(secret, PUBKEY, MODULUS)
+        return {"params": params, "encSecKey": encseckey}
+
+    @classmethod
+    def aes(cls, text, key):
+        pad = 16 - len(text) % 16
+        text = text + bytearray([pad] * pad)
+        encryptor = AES.new(key, 2, b"0102030405060708")
+        ciphertext = encryptor.encrypt(text)
+        return base64.b64encode(ciphertext)
+
+    @classmethod
+    def rsa(cls, text, pubkey, modulus):
+        text = text[::-1]
+        rs = pow(int(binascii.hexlify(text), 16), int(pubkey, 16), int(modulus, 16))
+        return format(rs, "x").zfill(256)
+
+    @classmethod
+    def create_key(cls, size):
+        return binascii.hexlify(os.urandom(size))[:16]
 
 
 search = netease_search
