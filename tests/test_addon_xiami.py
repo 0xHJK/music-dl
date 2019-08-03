493e99e514ec106c0285a073765d34c148f33675
HJK <HJKdev@gmail.com>
2019-08-03 13:35:01 +0800

    屏蔽部分单元测试

--- tests/test_addon_xiami.py
+++ tests/test_addon_xiami.py
@@ -10,6 +10,6 @@
 from music_dl.addons import xiami
 
 
-def test_xiami():
-    songs_list = xiami.search("好妹妹乐队")
-    assert songs_list is not None
+# def test_xiami():
+#     songs_list = xiami.search("好妹妹乐队")
+#     assert songs_list is not None
--- tests/test_source.py
+++ tests/test_source.py
@@ -12,17 +12,17 @@ from music_dl.source import MusicSource
 
 def test_search():
     ms = MusicSource()
-    songs_list = ms.search("五月天", ["baidu", "kugou", "netease", "qq"])
+    songs_list = ms.search("五月天", ["baidu"])
     assert songs_list is not None
 
 
-def test_single():
-    ms = MusicSource()
-    song = ms.single("https://music.163.com/#/song?id=26427663")
-    assert song is not None
-
-
-def test_playlist():
-    ms = MusicSource()
-    songs_list = ms.playlist("https://music.163.com/#/playlist?id=2602222983")
-    assert songs_list is not None
+# def test_single():
+#     ms = MusicSource()
+#     song = ms.single("https://music.163.com/#/song?id=26427663")
+#     assert song is not None
+#
+#
+# def test_playlist():
+#     ms = MusicSource()
+#     songs_list = ms.playlist("https://music.163.com/#/playlist?id=2602222983")
+#     assert songs_list is not None
