1c8274036423a7ed2b287e219b1bb55a02c4d9d7
张瑞 <2896402717@qq.com>
2024-11-24 01:11:15 +0800

    对搜索结果歌曲大小从大到小排序

--- music_dl/__main__.py
+++ music_dl/__main__.py
@@ -25,8 +25,6 @@ def menu(songs_list):
     tb.field_names = ["序号", "歌名", "歌手", "大小", "时长", "专辑", "来源"]
     # 遍历输出搜索列表
     songs_list.sort(key=lambda x: x.size)
-    for song in songs_list:
-        print(song.size)
     for index, song in enumerate(songs_list):
         song.idx = index
         tb.add_row(song.row)
